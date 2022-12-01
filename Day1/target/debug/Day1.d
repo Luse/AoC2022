f631e0962949820e700e78d7408369d443325f2f
Johan <johan@damm.io>
2022-12-01 07:55:44 +0000

    done with config

---
+++ Day1/target/debug/Day1.d
@@ -0,0 +1,1 @@
+/workspace/Day1/target/debug/Day1: /workspace/Day1/src/main.rs
---
+++ Day1/target/debug/deps/Day1-c107e6b734749bc6.d
@@ -0,0 +1,5 @@
+/workspace/Day1/target/debug/deps/Day1-c107e6b734749bc6: src/main.rs
+
+/workspace/Day1/target/debug/deps/Day1-c107e6b734749bc6.d: src/main.rs
+
+src/main.rs:
